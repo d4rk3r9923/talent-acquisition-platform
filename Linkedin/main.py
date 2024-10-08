f1d7584ebd2d15cb50797c0c737d5e2cdc1c0e22
Nguyen Duy Minh <89057446+Nguyen-Duy-Minh@users.noreply.github.com>
2024-10-08 09:25:26 +0700

    update Linkedin

--- Linkedin/main.py
+++ Linkedin/main.py
@@ -6,36 +6,51 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from tqdm import tqdm
 from time import sleep
 import pandas as pd
 import re
 from itertools import count
 import time
+import random
 
+
+# List of country-specific domains to rotate domains in get url
+DOMAINS = [
+    "us", "ca", "uk", "de", "fr", "au", "jp", "cn", "in", "br",
+    "ru", "it", "es", "mx", "kr", "se", "nl", "ch", "pl", "ar",
+    "za", "ng", "ie", "sg", "fi", "dk", "no", "hk", "tw", "il",
+    "ae", "at", "cz", "hu", "pt", "cl", "ro", "my", "th", "vn",
+    "ph", "co", "pe", "bg", "sk", "lt", "lv", "ee", "si", "by",
+    "is", "mt", "cy", "jo", "qa", "kh", "lk", "bd", "pk", "vn",
+    "ua", "kz", "uz", "am", "ge", "md", "mk", "ba", "hr", "rs",
+    "cy", "tt", "jm", "gh", "ke", "tz", "ug", "rw", "zm", "mw",
+    "zw", "lr", "sl", "tg", "sn", "ci", "cm", "ne", "ng", "dj"
+]
 
 # LinkedIn login function
 def loginLinkedIn(driver, username, password):
     driver.get("https://www.linkedin.com/login")
-    time.sleep(2)
-
+    time.sleep(5)
     driver.find_element(By.ID, "username").send_keys(username)
+    time.sleep(5)
     driver.find_element(By.ID, "password").send_keys(password)
+    time.sleep(5)
     driver.find_element(By.ID, "password").send_keys(Keys.RETURN)
-    time.sleep(2)
-    return driver
+    time.sleep(5)
 
-def expandSeeMore(driver):
+def expandSeeMore(driver):    
     last_height = driver.execute_script("return document.body.scrollHeight")
     
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.2)
+        time.sleep(5)
 
         see_more_buttons = driver.find_elements(By.XPATH, "//button[text()='…see more']")
         for button in see_more_buttons:
             try:
                 ActionChains(driver).move_to_element(button).click(button).perform()
-                time.sleep(0.2)  
+                time.sleep(5)  
             except Exception as e:
                 print(f"Error clicking 'See more' button: {e}")
 
@@ -43,6 +58,9 @@ def expandSeeMore(driver):
         if new_height == last_height:
             break
         last_height = new_height
+        time.sleep(5)
+
+        
 
 
 # Helper functions for cleaning data
@@ -84,16 +102,19 @@ def extractProfileData(driver):
     result_list = excludeStartingWith(result_list, "Activity")
     result_list = [cleanShowAllPattern(item) for item in result_list]
     result_list = extractSectionsFromList(result_list)
-
     return result_list
 
 
 # Scrape profile information and add URL
 def scrapeLinkedInProfile(driver, profileUrl):
+    profileUrl = profileUrl[:8] + random.choice(DOMAINS) + "." + profileUrl[8:]
+    time.sleep(10)
     driver.get(profileUrl)
-    time.sleep(1) 
+    time.sleep(10)
+    # removePopUp(driver)
+    # time.sleep(5)
     expandSeeMore(driver)
-
+    time.sleep(5) 
     profile_info = {
         "url": profileUrl,  # Include URL in the profile data
         "name": getTextContent(driver, By.CSS_SELECTOR, "h1.text-heading-xlarge"),
@@ -124,7 +145,7 @@ def mergeProfileData(dictList):
 def runLinkedInScraper(driver, df):
     try:
         all_profiles = []
-        for url in df["url"]:
+        for url in tqdm(df["url"]):
             try:
                 profile_info = scrapeLinkedInProfile(driver, url)
                 all_profiles.append(profile_info)
@@ -134,11 +155,19 @@ def runLinkedInScraper(driver, df):
         processed_profiles = mergeProfileData(all_profiles)
         profiles_df = pd.DataFrame(processed_profiles)
 
-        profiles_df.to_csv("data/linkedinProfiles.csv", index=False)
-        print(profiles_df)
+        # profiles_df.to_csv("linkedinProfiles.csv", index=False)
+        return profiles_df
     finally:
         driver.quit()
 
+
+# Click X on Pop-up to skip login in LinkedIn
+def removePopUp(driver):
+    try:
+        driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/button").click()
+    except:
+        pass
+    time.sleep(1)
 
 # Google search to collect LinkedIn profile URLs
 def collectProfileUrlsFromGoogle(driver, categories, locations, maxPage=2):
@@ -183,25 +212,36 @@ def collectProfileUrlsFromGoogle(driver, categories, locations, maxPage=2):
 # Script entry point
 if __name__ == "__main__":
     # Settings
-    categories = ["LLM"]
+    categories = ["AI Engineer", "HR", "Graphic Designer", "Finance", "Business"]
     locations = ["Ho Chi Minh"]
     max_page = 2
-    username = "hrai.aip391@gmail.com"
-    password = "hraiaip391"
+    username = "yaki81203@gmail.com"
+    password = "Nguyen113446779"
+    startpoint = 0
+    endpoint = 30
 
     # Initialize WebDriver with options
     options = Options()
     options.add_argument("start-maximized")
     options.add_argument("disable-infobars")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-extensions")
-    
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    service = Service(executable_path="chromedriver-win64/chromedriver.exe")
     driver = webdriver.Chrome(options=options)
+    time.sleep(5)
 
     # Collect LinkedIn profile URLs
-    profile_data = collectProfileUrlsFromGoogle(driver, categories, locations, max_page)
+    # profile_data = collectProfileUrlsFromGoogle(driver, categories, locations, max_page)
 
     # Log in to LinkedIn
-    driver = loginLinkedIn(driver, username=username, password=password)
+    loginLinkedIn(driver, username=username, password=password)
 
     # Scrape LinkedIn profiles and save the data
-    runLinkedInScraper(driver, profile_data)
+    profile_data = pd.read_csv("data/nguyen_dataset.csv")[startpoint:endpoint]
+    # print(profile_data)
+    df = runLinkedInScraper(driver, profile_data)
+    df.to_csv(f"dataset{startpoint}_{endpoint}.csv", index=False)
