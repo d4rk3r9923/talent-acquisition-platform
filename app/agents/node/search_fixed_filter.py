4680dcdeb117c1d9d5942c2cced62d24080f533d
d4rk3r <66075248+d4rk3r9923@users.noreply.github.com>
2024-11-10 08:07:29 +0000

    Refactor SearchFixedFilter class by removing unused imports and parameters, improving code clarity and structure

--- app/agents/node/search_fixed_filter.py
+++ app/agents/node/search_fixed_filter.py
@@ -1,45 +1,20 @@
 # input: json dict (from agent state)
 # output: list of ids -> to technical agent to analyze the candidates
-
-import json
-from enum import Enum
-
-from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import Runnable
-from langchain_openai import ChatOpenAI
 from loguru import logger
-from typing import Optional, TypedDict
-from pydantic import BaseModel, Field
-
-
 from app.agents.config import AgentState
-from app.agents.prompts import EXTRACT_ENTITY_PROMPT, EXTRACT_FIXED_ENTITY_PROMPT
-from app.preprocessing.schema import (
-    PositionName, 
-    DegreeCategory, 
-    SkillName,
-    CertificationCategory,
-    PublicationCategory,
-)
-
 from neo4j import GraphDatabase
 
-
-
-
-
 class SearchFixedFilter(Runnable):
-    def __init__(self, llm: ChatOpenAI, state: AgentState) -> None:
+    def __init__(self, state: AgentState) -> None:
         super().__init__()
-        self.llm = llm
-        self.graphdb = GraphDatabase.driver(uri=state['uri'], 
-                                            database=state['database'],
-                                            auth=(state['user'],state['password'])
-                                        )
-        
+        self.graphdb = GraphDatabase.driver(
+            uri=state['uri'],
+            database=state['database'],
+            auth=(state['user'],state['password'])
+            )
 
     def _filter_fixed_nodes(self, data):
-        
         query = "MATCH (p:Person)\n"
 
         # Fixed filters
@@ -77,11 +52,10 @@ class SearchFixedFilter(Runnable):
             query += f"{data['years_of_experience'].strip()}\n"
 
         query += "RETURN DISTINCT(p) LIMIT 10"
-        
+
         logger.info(f"Generated query:\n{query}")
         return query
         
-    # TODOS: Async llm invoke
     def invoke(self, state: AgentState) -> dict:
         data = {
             "roles": state['role'],
@@ -89,14 +63,13 @@ class SearchFixedFilter(Runnable):
             "certification": state['certification'],
             "education_degree": state['education_degree'],
             "age": state['age'],
-            "years_of_experience": state['years_of_experience'],
+            "years_of_experience": state['years_of_experience']
         }
         with self.graphdb.session(database=state['database']) as session:
-    
             query = self._filter_fixed_nodes(data)
             result = session.run(query)
 
             for record in result:
                 logger.info(f"Found person node: {record['p']}")
-        
+
         self.graphdb.close()
